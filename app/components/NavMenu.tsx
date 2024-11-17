d009f2f6bc86c38cf8a299a24738f66f01ae5971
essss <sousououederni@gmail.com>
2024-11-17 19:27:24 +0200

    using protected routes

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -1,5 +1,10 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const ACTIVE_ROUTE = "bg-gray-700"
+const INACTIVE_ROUTE = "hover:text-gray-400"
 
 function AuthButton(){
     const {data : session} = useSession();
@@ -21,9 +26,19 @@ function AuthButton(){
 }
 
 export default function NavMenu(){
+    const pathName = usePathname();
     return(
         <div>
             <AuthButton />
+            <br/>
+            <ul>
+                <Link href="/">
+                    <li className={pathName === "/" ? ACTIVE_ROUTE: INACTIVE_ROUTE }> Home </li>
+                </Link>
+                <Link href="/protected">
+                    <li className={pathName === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE}> Protected Route</li>
+                </Link>
+            </ul>
         </div>
     )
 }
