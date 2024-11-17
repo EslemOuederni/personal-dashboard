5ef9364ed6b2909ddd17eedf389a897b57f33e0b
essss <sousououederni@gmail.com>
2024-11-17 14:52:27 +0200

    navmenu auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,29 @@
 import "./globals.css";
+import { getServerSession } from "next-auth";
+import SessionProvider from "./components/SessionProvider";
+import { Metadata } from "next";
+import NavMenu from "./components/NavMenu";
 
-export default function RootLayout({
+export const metadata: Metadata ={
+  title: 'Personal Dashboard App',
+  description: "A personal dashboard App to manage your projects and tasks"
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body>
-        {children}
+       <SessionProvider session={session}>
+          <NavMenu />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
