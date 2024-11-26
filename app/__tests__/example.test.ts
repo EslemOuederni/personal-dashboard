9451f60aafd2ef40fb70e2ca48cbb5af9a7c89ba
essss <sousououederni@gmail.com>
2024-11-26 17:58:46 +0200

    added jest

--- app/__tests__/example.test.ts
+++ app/__tests__/example.test.ts
@@ -1,7 +1,7 @@
-import {describe, expect, test} from '@jest/globals';
+import '@testing-library/jest-dom'
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
-  });
-});
+describe('sum', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
