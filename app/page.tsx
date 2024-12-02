e3e4b97e672d1b4ff77897cf39741f7dd38b5e71
essss <sousououederni@gmail.com>
2024-12-02 13:44:47 +0200

    updated authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../lib/auth";
-
+import Dashboard from '../components/dashboard/page';
+import RedirectButton from '@/components/RedirectBtn';
 export default async function Home () {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
   return (
     <>
-      <h1>Personal Dashboard App</h1>
-      <p>A personal dashboard App to manage your projects and tasks</p>
+     {session ? (
+      <Dashboard />
+     )
+    : (
+      <div>
+        <h1>You&apos;re not logged in</h1>
+        <RedirectButton />
+      </div>
+      )}
     </>
   );
 }
