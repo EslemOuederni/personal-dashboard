598b3b60ff1bfda0441538ea1ff1726e2142e01e
essss <sousououederni@gmail.com>
2024-12-13 23:05:21 +0200

    updated the layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from "next";
+import NavBar from "@/components/Navbar";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar";
+import Header from "@/components/Header";
+import { auth } from '@/lib/auth';
+import { SessionProvider } from 'next-auth/react';
+
+export const metadata: Metadata = {
+    title: "Personal Dashboard App",
+    description: "A personal dashboard App to manage your projects and tasks",
+};
+
+export default async function RootLayout ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const session = await auth()
+    return (
+        <html lang="en">
+            <body className="overflow-y-hidden">
+                <SessionProvider session={session}>
+                    <SidebarProvider>
+                        <NavBar />
+                        <div className="flex flex-col w-full h-screen p-2">
+                            <header className="bg-white h-[12%] fixed top-0 left-0 z-30 flex items-center sm:sticky rounded-tl-[18px] rounded-tr-[18px] border-b border-[#DBDBDB] sm:border-1 min-[300px]:sticky ">
+                                <Header />
+                            </header>
+                            <main className="bg-white items-start h-[88%] gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 overflow-y-scroll rounded-br-[18px] rounded-bl-[18px] border-t-0 ">
+                                {children}
+                            </main>
+                        </div>
+                    </SidebarProvider>
+                </SessionProvider>
+            </body>
+        </html>
+    );
+}
--- components/dashboard/page.tsx
+++ components/dashboard/page.tsx
@@ -9,23 +9,9 @@ import Header from "../Header";
 
 export const dynamic = "force-dynamic";
 
-export default function Dashboard() {
+export default function Dashboard () {
   const { data: session, status } = useSession();
   const router = useRouter();
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.replace("/sign-in");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  if (!session) {
-    return null; // Avoid rendering anything until redirection completes
-  }
 
   return (
     <div className="flex flex-col justify-start ">
