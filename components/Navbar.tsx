7c6a172943f497bdc012449c0824ec34510d7861
essss <sousououederni@gmail.com>
2024-12-18 16:11:17 +0200

    small fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,16 +17,16 @@ const NavBar = () => {
   return (
     <Sidebar variant="inset" collapsible="icon" className="bg-[#1F271B]">
       <SidebarHeader className=" h-[12%]">
-        <Link href="/" className="px-0 mx-1">
+        <Link href="/dashboard" className="px-0 mx-1">
           <Image src="/logo-1.svg" alt="Logo" width={127} height={40.5} />
         </Link>
       </SidebarHeader>
       <SidebarContent className="pt-4">
-        <NavMain/>
+        <NavMain />
       </SidebarContent>
       <hr className="w-[calc(100%-34px)] mx-auto  border-t border-[#DBDBDB] " />
       <SidebarContent className="flex-1 pt-2">
-        <NavProjects  />
+        <NavProjects />
       </SidebarContent>
     </Sidebar>
   );
