27034c3dfde3ca2a4d89e87dfb395f0f4868d3e3
essss <sousououederni@gmail.com>
2024-12-25 20:36:42 +0200

    format date function

--- app/dashboard/project/[id]/page.tsx
+++ app/dashboard/project/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getProjectById } from '../../actions';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { date } from 'zod';
 
 export default async function Page ({
     params,
@@ -14,11 +15,14 @@ export default async function Page ({
         return <div>Project not found</div>
     }
 
+    const formatDate = (date?: Date) => (date ? new Date(date).toLocaleDateString() : "N/A");
+
     return (
         <Table>
             <TableCaption>{project.name}</TableCaption>
             <TableHeader>
                 <TableRow>
+                    <TableHead>Project Name</TableHead>
                     <TableHead className="w-[100px]">Start Date</TableHead>
                     <TableHead>End Date</TableHead>
                     <TableHead>Tag</TableHead>
@@ -28,11 +32,12 @@ export default async function Page ({
             </TableHeader>
             <TableBody>
                 <TableRow>
-                    <TableCell className="font-medium">{project.startDate?.toString()}</TableCell>
-                    <TableCell>{project.endDate?.toString()}</TableCell>
-                    <TableCell>{project.tag}</TableCell>
+                    <TableCell className="font-medium">{project.name}</TableCell>
+                    <TableCell className="font-medium">{formatDate(project.startDate)}</TableCell>
+                    <TableCell>{formatDate(project.endDate)}</TableCell>
+                    <TableCell>{project?.tag}</TableCell>
                     <TableCell>{project.status}</TableCell>
-                    <TableCell className="text-right">{project.tasks?.length}</TableCell>
+                    <TableCell className="text-center">{project.tasks?.length}</TableCell>
                 </TableRow>
             </TableBody>
         </Table>
