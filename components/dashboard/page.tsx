714ecf7d3bd96b0e2c744e4c65aea200ae2f599c
essss <sousououederni@gmail.com>
2025-01-08 21:49:30 +0200

    added currenttime and start tracking time

--- components/dashboard/page.tsx
+++ components/dashboard/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import CurrentDay from './currentDay';
+import StartTimerTracker from './startTimerTracker';
+
 export const dynamic = "force-dynamic";
 
 export default function Dashboard () {
-
-
   return (
-    <div className="flex flex-col justify-start ">
+    <div className="flex flex-col justify-start pt-3 ">
+      <div className='flex flex-row justify-between items-center'>
+        <CurrentDay />
+        <StartTimerTracker />
+      </div>
     </div>
   );
 }
